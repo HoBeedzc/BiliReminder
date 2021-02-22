12391a2d091ff1f04fdd700cb80619d698b1c2b6
HoBee <lzqpublic@163.com>
2021-02-22 22:38:28 +0800

    update config

--- bili_reminder/config.py
+++ bili_reminder/config.py
@@ -1,4 +1,23 @@
 HEAD = {
-        'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-    }
+    'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+}
+
+SMTP_SERVER = 'smtp.example.com'
+PORT = 465
+FROM_ADDR = 'example@from.com'
+PASSWD = 'yourpassword'
+TO_ADDR = 'example@to.com'
+
+BILIBILI_UID = '' # your uid
+BILIBILI_COOKIES = {}
+
+CONTENT = '''
+你关注的up主更新了！
+up主:{},
+稿件:{},
+点此可直接观看:{}
+'''
+
+SLEEP_TIME = 60
+MAX_PAGE = 5
