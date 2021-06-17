17dc40c19ca7b5285e94b9c0eab2a3cb09dd0831
hobee_mac <lzqpublic@163.com>
2021-06-17 12:12:10 +0800

    modify user info url

--- BiliReminder/Apps/User/urls.py
+++ BiliReminder/Apps/User/urls.py
@@ -6,12 +6,11 @@ app_name = 'Apps.User'
 urlpatterns = [
     path('', views.empty_redirect),
     path('index/', views.index, name='index'),
-    path('info/<int:uid>', views.info, name='info'),
-    path('edit/',views.edit, name='edit'),
-    path('vip/',views.vip, name='vip'),
-    path('logoff/',views.logoff, name='logoff'),
+    path('info/', views.info, name='info'),
+    path('edit/', views.edit, name='edit'),
+    path('vip/', views.vip, name='vip'),
+    path('logoff/', views.LogoffView.as_view(), name='logoff'),
 
-    
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
